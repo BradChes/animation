959dbd03d3a8bb644f2a56666b9e4f14445bfdb1
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-03-28 14:49:29 +0000

    Initial Commit

---
+++ animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animation
+//
+//  Created by Bradley Chesworth on 28/03/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
